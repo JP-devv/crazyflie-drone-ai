d7cce0dcc52e3917f4393ed9b4402dc5d47ec4ec
Johann <johann.plaster@icloud.com>
2023-05-04 20:40:22 -0500

    device set to default instead of mps

--- drone.py
+++ drone.py
@@ -6,4 +6,4 @@ if len(sys.argv) > 1:
 else:
   image = './data/images/bus.jpg'
 
-os.system(f'python detect.py --weights yolov5s.pt --class 0 --save-txt --device mps --source {image}')
+os.system(f'python detect.py --weights yolov5s.pt --class 0 --save-txt --source {image}')
